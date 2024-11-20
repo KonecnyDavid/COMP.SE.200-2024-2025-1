2b72ac3c52cc8a1591a7958e77504a17c446305a
Ali Mir <ali.mir@tuni.fi>
2024-11-20 03:52:13 +0200

    filter.js tests added

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
 import add from '../src/add.js';
 import eq from '../src/eq.js';
+import filter from '../src/filter.js';
 
 describe('Add Function', () => {
   it('should add two positive numbers correctly', () => {
@@ -53,3 +54,29 @@ describe('Equals Function', () => {
       expect(eq(obj, obj)).to.be.true;
     });
   });
+  
+  describe('Filter Function', () => {
+      it('should filter an array based on a truthy predicate', () => {
+        const users = [{ user: 'Ali', active: true }, { user: 'David', active: false }];
+        const result = filter(users, ({ active }) => active);
+        expect(result).to.deep.equal([{ user: 'Ali', active: true }]);
+      });
+    
+      it('should return a nested empty array if no elements match', () => {
+        const numbers = [1, 2, 3];
+        const result = filter(numbers, num => num > 3);
+        expect(result).to.deep.equal([[]]);
+      });
+    
+      it('should return the original array if all elements match', () => {
+        const numbers = [1, 2, 3];
+        const result = filter(numbers, num => num > 0);
+        expect(result).to.deep.equal(numbers);
+      });
+    
+      it('should return a nested empty array for an empty input array', () => {
+        const result = filter([], () => true);
+        expect(result).to.deep.equal([[]]);
+      });
+    });
+  
