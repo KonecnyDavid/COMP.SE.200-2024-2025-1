4f822c81c7b020e3bd08930120a191034321bc7a
Ali Mir <ali.mir@tuni.fi>
2024-11-20 03:51:00 +0200

    tests for eq.js

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 import { expect } from 'chai';
 import add from '../src/add.js';
+import eq from '../src/eq.js';
 
 describe('Add Function', () => {
   it('should add two positive numbers correctly', () => {
@@ -23,3 +24,32 @@ describe('Add Function', () => {
     expect(add(0.1, 0.2)).to.be.closeTo(0.3, 0.0001);
   });
 });
+
+describe('Equals Function', () => {
+    it('should return true for equivalent numbers', () => {
+      expect(eq(1, 1)).to.be.true;
+    });
+  
+    it('should return false for different numbers', () => {
+      expect(eq(1, 2)).to.be.false;
+    });
+  
+    it('should return true for NaN compared to NaN', () => {
+      expect(eq(NaN, NaN)).to.be.true;
+    });
+  
+    it('should return true for equivalent strings', () => {
+      expect(eq('abc', 'abc')).to.be.true;
+    });
+  
+    it('should return false for different objects', () => {
+      const obj1 = { a: 1 };
+      const obj2 = { a: 1 };
+      expect(eq(obj1, obj2)).to.be.false;
+    });
+  
+    it('should return true for the same object', () => {
+      const obj = { a: 1 };
+      expect(eq(obj, obj)).to.be.true;
+    });
+  });
